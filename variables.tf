8e3ae9fa72adb35f3fb953548df503caeffa87ca
DIJSAA <derekijackson89@gmail.com>
2026-05-11 11:43:18 -0400

    Phase 1: initial Terraform structure and provider configuration

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix for all resources"
+  type        = string
+  default     = "firewall-alb"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of availability zones to use"
+  type        = number
+  default     = 2
+}
